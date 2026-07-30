384da7311e1090459b6a6aab192e3cb9a2ac25c4
sikindra97 <sikindramahto374@gmail.com>
2026-07-30 14:51:50 +0530

    Time: 11 ms (91.61%), Space: 22.1 MB (47.55%) - LeetHub

---
+++ 1410-html-entity-parser/1410-html-entity-parser.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    unordered_map<string, char> mp = {
+        {"&quot;", '"'},
+        {"&apos;", '\''},
+        {"&amp;", '&'},
+        {"&gt;", '>'},
+        {"&lt;", '<'},
+        {"&frasl;", '/'}
+    };
+
+    string entityParser(string text) {
+
+        string ans = "";
+
+        for (int i = 0; i < text.size(); i++) {
+
+            if (text[i] == '&') {
+
+                string res = "";
+                int j = i;
+
+                while (j < text.size() && text[j] != ';') {
+                    res += text[j];
+                    j++;
+                }
+
+                if (j < text.size()) {
+                    res += ';';
+                }
+
+                if (mp.count(res)) {
+                    ans += mp[res];
+                    i = j;          // Skip the whole entity
+                } else {
+                    ans += text[i]; // Not a valid entity
+                }
+            }
+            else {
+                ans += text[i];
+            }
+        }
+
+        return ans;
+    }
+};
