6e710889b5a1b57114d918c18ea005c06ec3dffb
sikindra97 <sikindramahto374@gmail.com>
2026-09-23 23:59:50 +0530

    Time: 622 ms (60.29%), Space: 0B (100%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,5 @@
+select employee_id from Employees  where employee_id not in(select employee_id  from Salaries  )
+UNION
+select employee_id from Salaries  where employee_id not in(select employee_id  from Employees  ) 
+
+order by employee_id asc
