e8b1091a3507bbd1d40bef3c2f3f8f4fc0016f64
sikindra97 <sikindramahto374@gmail.com>
2026-09-25 16:24:42 +0530

    Time: 5 ms (13.39%), Space: 43.8 MB (7.32%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character>st=new Stack<>();
+      for(int i=0;i<s.length();i++){
+        if(!st.isEmpty() && st.peek()=='(' && s.charAt(i)==')'){
+            st.pop();
+        }
+        else if(!st.isEmpty() && st.peek()=='{' && s.charAt(i)=='}'){
+            st.pop();
+        }else if(!st.isEmpty() && st.peek()=='[' && s.charAt(i)==']'){
+            st.pop();
+        }else{
+            st.push(s.charAt(i));
+        }
+      }  
+      return st.isEmpty();
+    }
+}
